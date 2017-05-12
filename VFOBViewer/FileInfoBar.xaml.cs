00318819e499a9d4f0de99f31c3c4172067f855b
WYQ <194897868@qq.com>
2017-05-12 22:15:53 +0800

    Init Project

---
+++ VFOBViewer/FileInfoBar.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WYQDoNet.DoNet.IO.DirectoryTools;
+
+namespace VFOBViewer
+{
+    /// <summary>
+    /// FileInfoBar.xaml 的交互逻辑
+    /// </summary>
+    public partial class FileInfoBar : UserControl
+    {
+        Dictionary<string, string> ExtImg = new Dictionary<string, string>();
+
+        public static FileInfoBar fib;
+        public FileInfoBar()
+        {
+            InitializeComponent();
+        }
+
+        public FileModel FileInfo;
+        public FileObj fileObj;
+        public FileInfoBar(FileModel fm, FileObj fileObj)
+        {
+            AddExtImg();
+            InitializeComponent();
+            FileInfo = fm;
+            this.fileObj = fileObj;
+            TextBox_Name.Text = FileInfo.Name;
+            Label_LastWrite.Content = FileInfo.LastWriteTime;
+            if (fm.IsFile)
+                Label_Length.Content = WYQDoNet.DoNet.Base.String.StringTools.ConvertBytes(FileInfo.Length, 2);
+            else
+            {
+                Label_Length.Content = fileObj.GetDirFileModels(fm.FullPath).Length + "文件";
+            }
+            SetImage();
+            this.ToolTip = "创建日期：" + FileInfo.CreationTime + Environment.NewLine + "访问日期：" + FileInfo.LastAccessTime;
+        }
+        public void SetImage()
+        {
+            if (FileInfo.IsFile)
+            {
+                BitmapImage bi = new BitmapImage(new Uri("pack://application:,,,/Images/File/format_unkown.png"));
+                string ext = FileInfo.Extension;
+                if (!string.IsNullOrWhiteSpace(ext))
+                {
+                    foreach (var key in ExtImg.Keys)
+                    {
+                        if (ext.Trim().ToLower().Contains(key))
+                        {
+                            bi = new BitmapImage(new Uri("pack://application:,,,/Images/File/" + ExtImg[key]));
+                        }
+                    }
+                }
+                Image_Format.Source = bi;
+            }
+        }
+
+        public void AddExtImg()
+        {
+            ExtImg.Add("txt", "format_text.png");
+            ExtImg.Add("doc", "format_word.png");
+            ExtImg.Add("xls", "format_excel.png");
+            ExtImg.Add("pdf", "format_pdf.png");
+            ExtImg.Add("zip", "format_zip.png");
+            ExtImg.Add("rar", "format_zip.png");
+            ExtImg.Add("ppt", "format_ppt.png");
+            ExtImg.Add("chm", "format_chm.png");
+            ExtImg.Add("unkown", "format_unkown.png");
+
+            ExtImg.Add("mp4", "format_media.png");
+            ExtImg.Add("avi", "format_media.png");
+            ExtImg.Add("flv", "format_media.png");
+            ExtImg.Add("rm", "format_media.png");
+            ExtImg.Add("rmvb", "format_media.png");
+            ExtImg.Add("wmv", "format_media.png");
+            ExtImg.Add("mkv", "format_media.png");
+
+
+            ExtImg.Add("mp3", "format_music.png");
+            ExtImg.Add("wav", "format_music.png");
+            ExtImg.Add("ogg", "format_music.png");
+
+
+            ExtImg.Add("png", "format_picture.png");
+            ExtImg.Add("bmp", "format_picture.png");
+            ExtImg.Add("jpg", "format_picture.png");
+            ExtImg.Add("gif", "format_picture.png");
+        }
+
+        private void UserControl_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (fib != null) fib.Background = new SolidColorBrush(Color.FromArgb(255, 255, 255, 255));
+            this.Background = new SolidColorBrush(Color.FromArgb(100, 0, 255, 0));
+            fib = this;
+        }
+    }
+}
---
+++ VFOBViewer/MainWindow.xaml.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WYQDoNet.DoNet.IO.DirectoryTools;
+
+namespace VFOBViewer
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string FOBPath = "";
+        public bool ShowDialog = false;
+        public FileObj fob;
+
+        private string currentPath;
+
+        public string CurrentPath
+        {
+            get
+            {
+                return currentPath;
+            }
+
+            set
+            {
+                currentPath = value;
+                TextBox_CurrentPath.Text = value;
+                AddFileModels(fob.GetDirFileModels(value));
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            if (Environment.GetCommandLineArgs().Length > 1)
+                FOBPath = Environment.GetCommandLineArgs()[1].Trim();
+        }
+
+        private bool ViewBox = false;
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            FileDirDialog_FOB.TextBoxPath.IsReadOnly = true;
+            FileDirDialog_FOB.TextBoxPath.TextChanged += TextBoxPath_TextChanged;
+            FileDirDialog_NeedFOB.TextBoxPath.TextChanged -= FileDirDialog_NeedFOB.TextBoxPath_TextChanged;
+            FileDirDialog_NeedFOB.ButtonBrowser.Click -= FileDirDialog_NeedFOB.ButtonBrowser_Click;
+            FileDirDialog_NeedFOB.ButtonBrowser.Click += NFOB_ButtonBrowser_Click;
+            if (!string.IsNullOrWhiteSpace(FOBPath))
+            {
+                ShowDialog = false;
+                FileDirDialog_FOB.TextBoxPath.Text = FOBPath;
+                fob = WYQDoNet.DoNet.IO.ObjectUtils.ObjectSerializationHelper.BinaryDeserialize<FileObj>(FOBPath);
+                CurrentPath = fob.Root;
+            }
+            if (ShowDialog)
+                FileDirDialog_FOB.ButtonBrowser.RaiseEvent(new RoutedEventArgs(Button.ClickEvent, FileDirDialog_FOB.ButtonBrowser));
+        }
+
+        private void NFOB_ButtonBrowser_Click(object sender, RoutedEventArgs e)
+        {
+            string dir = FileDirDialog_NeedFOB.ShowFolderBrowserDialog("请选择需要序列化文件夹...");
+            if (string.IsNullOrWhiteSpace(dir)) return;
+            string tbPath = FileDirDialog_NeedFOB.TextBoxPath.Text;
+
+            if (string.IsNullOrWhiteSpace(tbPath)) FileDirDialog_NeedFOB.TextBoxPath.Text = "";
+
+            FileDirDialog_NeedFOB.TextBoxPath.Text += dir + ";";
+        }
+
+
+
+        private void TextBoxPath_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            fob = WYQDoNet.DoNet.IO.ObjectUtils.ObjectSerializationHelper.BinaryDeserialize<FileObj>(tb.Text);
+
+            CurrentPath = fob.Root;
+        }
+
+        public void AddFileModels(FileModel[] fms)
+        {
+            WrapPanel_FileInfos.Children.Clear();
+
+            if (ViewBox)
+                foreach (var fm in fms)
+                {
+                    FileInfoBox fib = new FileInfoBox(fm, fob);
+
+                    WrapPanel_FileInfos.Children.Add(fib);
+
+                    fib.MouseDoubleClick += Fib_MouseDoubleClick;
+                }
+            else
+                foreach (var fm in fms)
+                {
+                    FileInfoBar fib = new FileInfoBar(fm, fob);
+
+                    WrapPanel_FileInfos.Children.Add(fib);
+
+                    fib.MouseDoubleClick += Fib_MouseDoubleClick;
+                }
+        }
+
+        public void AppendFileModels(FileModel[] fms)
+        {
+            foreach (var fm in fms)
+            {
+                if (ViewBox)
+                {
+                    FileInfoBox fib = new FileInfoBox(fm, fob);
+
+                    WrapPanel_FileInfos.Children.Add(fib);
+
+                    fib.MouseDoubleClick += Fib_MouseDoubleClick;
+                }
+                else
+                {
+                    FileInfoBar fib = new FileInfoBar(fm, fob);
+
+                    WrapPanel_FileInfos.Children.Add(fib);
+
+                    fib.MouseDoubleClick += Fib_MouseDoubleClick;
+                }
+            }
+        }
+        private void Fib_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (ViewBox)
+            {
+                FileInfoBox fib = (FileInfoBox)sender;
+
+                if (fib.FileInfo.IsFile)
+                {
+
+                }
+                else
+                {
+                    CurrentPath = fib.FileInfo.FullPath;
+                }
+            }
+            else
+            {
+                FileInfoBar fib = (FileInfoBar)sender;
+
+                if (fib.FileInfo.IsFile)
+                {
+
+                }
+                else
+                {
+                    CurrentPath = fib.FileInfo.FullPath;
+                }
+            }
+
+        }
+
+        private void Button_Parent_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                CurrentPath = fob.GetDirectory(CurrentPath).ParentDirectory;
+            }
+            catch
+            {
+                CurrentPath = fob.Root;
+            }
+        }
+
+        private void Button_Root_Click(object sender, RoutedEventArgs e)
+        {
+            CurrentPath = fob.Root;
+        }
+
+        private void Button_ViewChange_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(CurrentPath)) return;
+            if (ViewBox) ViewBox = false;
+            else ViewBox = true;
+            CurrentPath = CurrentPath;
+        }
+
+        private void Button_LocatePath_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(TextBox_CurrentPath.Text))
+                CurrentPath = TextBox_CurrentPath.Text;
+        }
+
+        private void Button_StartFOB_Click(object sender, RoutedEventArgs e)
+        {
+            string path = FileDirDialog_NeedFOB.TextBoxPath.Text;
+
+            if (string.IsNullOrWhiteSpace(path)) return;
+            string[] dirs = path.Trim().Split(new string[] { ";" }, StringSplitOptions.RemoveEmptyEntries);
+
+            string savePath = FileDirDialog_NeedFOB.SaveFileDialog("选择保存文件位置...", "VFOB目录|*.vfob");
+            if (string.IsNullOrWhiteSpace(savePath)) return;
+            List<string> ldirs = dirs.ToList();
+            ldirs.RemoveAt(0);
+            WYQDoNet.DoNet.IO.DirectoryTools.DirectoryUtils.SaveDirFilesBinFile(dirs[0], ldirs.ToArray(), savePath);
+
+            MessageBox.Show("序列化完成!!!");
+        }
+
+        private void Button_Search_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(FileDirDialog_FOB.TextBoxPath.Text)) return;
+            if (TextBox_Search.Text.TrimStart().StartsWith("R:"))
+                AddFileModels(fob.SearchDirFiles(TextBox_Search.Text.TrimStart().Remove(0, 2), true));
+            else
+                AddFileModels(fob.SearchDirFiles(TextBox_Search.Text, false));
+        }
+    }
+}
